26179f841f054a4455407e983c038eab6a6243e0
xuqiang <495219869@qq.com>
2013-10-10 09:24:52 +0800

    Initial Commit

---
+++ URLToCanUseUrl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  URLToCanUseUrl
+//
+//  Created by xuqiang on 13-10-10.
+//  Copyright (c) 2013年 xuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URLToCanUseUrl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  URLToCanUseUrl
+//
+//  Created by xuqiang on 13-10-10.
+//  Copyright (c) 2013年 xuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
